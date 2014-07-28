587c8439f37dbadd10ffcb5e7f95f3c90d8d319c
Joshua Partogi <proscrumtrainer@live.com>
2014-07-28 10:54:04 +0800

    Initial commit

---
+++ MovieRentalTest/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieRental
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestStatement()
+        {
+            Movie movie = new Movie("Transformer", Movie.REGULAR);
+
+            Rental rental = new Rental(movie, 3);
+
+            Customer customer = new Customer("jpartogi");
+            customer.AddRental(rental);
+
+            String statement = customer.Statement();
+            StringAssert.Contains(statement, "Transformer");
+        }
+    }
+}
